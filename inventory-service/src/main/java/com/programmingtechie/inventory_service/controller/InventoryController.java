c62a89da5143affd86365ca6ecacc74258893372
Ansh Makker <amakker@griddynamics.com>
2024-07-03 16:02:48 +0530

    added 3 services

---
+++ inventory-service/src/main/java/com/programmingtechie/inventory_service/controller/InventoryController.java
@@ -0,0 +1,23 @@
+package com.programmingtechie.inventory_service.controller;
+
+import com.programmingtechie.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
